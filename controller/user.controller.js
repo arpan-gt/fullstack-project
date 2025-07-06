a15162de75381013fe53b99489cd392571a9aa21
arpan-gt <arpan.main@outlook.com>
2025-07-06 16:02:42 +0530

    login function implementation

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -100,5 +100,58 @@ const verifyUser = async (req, res) => {
   await user.save();
 };
 
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({
+      message: "All fields are required",
+    });
+  }
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({
+        message: "Invalid email or password",
+      });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    console.log(isMatch);
+
+    if (!isMatch) {
+      return res.status(400).json({
+        message: "Invalid email or password",
+      });
+    }
+
+    jwt.sign({ id: user._id, role: user.role }, "shhhhh", {
+      expiresIn: "24h",
+    });
+    const cookieOptions = {
+      httpOnly: true,
+      secure: true,
+      maxAge: 24 * 60 * 60 * 1000,
+    };
+
+    res.cookie("token", token, {});
+    res.status(200).json({
+      succuees: true,
+      message: "Login Successfull",
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        role: user.role,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({
+      message: "Something Wrong",
+    });
+  }
+};
 
 export { registerUser, verifyUser };
